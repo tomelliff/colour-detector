2dfbe15e9286f3712d4cc521c7db835b3876850b
Tom Elliff <tomelliff@gmail.com>
2016-12-18 12:21:11 +0000

    Add function to get the product from the datastore by its RGB values
    Need to refactor into class, add tests (mocking the call to ColourDetector to avoid the Google Vision API call?) and think about interfaces a little.

--- data_storage.py
+++ data_storage.py
@@ -10,7 +10,7 @@ def _format_rgb_values(rgb_values):
 def _zero_pad_number(number, length=3):
     return str(number).zfill(length)
 
-def store_rgbs_for_product(rgb, product_code):
+def store_rgb_for_product(rgb, product_code):
     """
     Store the RGB values against the product code.
     For now this is just a flat CSV file.
@@ -20,14 +20,28 @@ def store_rgbs_for_product(rgb, product_code):
     with open('database.txt', 'a') as database:
         database.write('{},{}\n'.format(rgb_values, product_code))
 
+def get_product_for_rgb(rgb):
+    """
+    Get the product code for an inputted RGB tuple.
+    For now this is just from a flat CSV file.
+    """
+
+    rgb_values = _format_rgb_values(rgb)
+    with open('database.txt', 'r') as database:
+        lines = [line.rstrip('\n') for line in database.readlines()]
+        for line in lines:
+            if line.startswith(rgb_values):
+                return line.split(',')[1]
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('image_file', help='The image you\'d like to get the colour for.')
-    parser.add_argument('product_code', help='The product code for the product')
-    args = parser.parse_args()
-
-    cd = colour_detector.ColourDetector()
-    rgb = cd.get_rgb_values(args.image_file)
-
-    store_rgbs_for_product(rgb, args.product_code)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('image_file', help='The image you\'d like to get the colour for.')
+    # parser.add_argument('product_code', help='The product code for the product')
+    # args = parser.parse_args()
+    #
+    # cd = colour_detector.ColourDetector()
+    # rgb = cd.get_rgb_values(args.image_file)
+    #
+    # store_rgb_for_product(rgb, args.product_code)
+    print(get_product_for_rgb((106,82,96)))
