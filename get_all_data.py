ad2fca3f60b7957867c2aa4d39349ae61b4006b9
Tom Elliff <tomelliff@gmail.com>
2017-01-01 20:05:27 +0000

    Remove get_all_data command line script

--- get_all_data.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-'''
-Command line script for retrieving all stored RGB values in the database.
-'''
-import data_storage
-
-ds = data_storage.DataStorage()
-print(ds.get_all_rgb_values())
